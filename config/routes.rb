3bf2fb69409196c10f9e7170b690eb5082db0367
soroosh-gb <soroosh.gb@gmail.com>
2020-09-03 20:19:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :application, only: [:index, :create]
+  root 'products#index'
+  post '/' => 'products#add'
+  resources :products 
+
 end
